76bbabfc9e354404b009a23e2f985a0ad1b94020
Amrutha-01 <amrutharokkam573@gmail.com>
2024-05-11 22:25:20 +0530

    initial commit

--- src/components/AuthPage.js
+++ src/components/AuthPage.js
@@ -4,6 +4,7 @@ import Signup from "./Signup";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 const AuthPage = () => {
+  
   return (
     <div className="flex justify-center items-center mt-36">
       <Tabs className="bg-black text-white h-80 w-2/5 sm:w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3">
